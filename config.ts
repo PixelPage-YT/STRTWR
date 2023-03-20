1df21213b91b1c4655c68f3327cc90a8193312b8
PixelPage <80278171+PixelPage-YT@users.noreply.github.com>
2023-03-20 20:13:25 +0100

    🧹 fix: remove non-existent store

--- config.ts
+++ config.ts
@@ -262,11 +262,6 @@ const config = {
                     link: 'https://www.pivot-skateshop.de/',
                 },
                 {
-                    name: 'The good will out',
-                    description: 'Sneaker, ...',
-                    link: 'https://www.thegoodwillout.com/',
-                },
-                {
                     name: 'Nigh',
                     description: 'Sneaker, ...',
                     link: 'https://www.nighshop.com/',
